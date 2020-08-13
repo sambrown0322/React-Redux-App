c296682f9ad6e729b40d0121169206b47db9194c
David Brown <davidbrown@Davids-iMac.local>
2020-08-13 15:00:41 -0600

    mvp

--- async-app/src/components/Joke.js
+++ async-app/src/components/Joke.js
@@ -1,4 +1,12 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 20%;
+  margin-bottom: 2%;
+  border: 2px solid black;
+  box-shadow: 5px 5px 5px grey;
+`;
 
 export default function Joke(props) {
   const [answer, setAnswer] = useState(false);
@@ -6,10 +14,14 @@ export default function Joke(props) {
     setAnswer(!answer);
   };
   return (
-    <div key={props.joke.id}>
+    <Card onClick={revealAnswer} key={props.joke.id}>
       <h3>{props.joke.setup}</h3>
-      {answer ? <p>{props.joke.punchline}</p> : null}
-      <button onClick={revealAnswer}>Answer</button>
-    </div>
+      {answer ? (
+        <p>{props.joke.punchline}</p>
+      ) : // <button className="cursor" onClick={revealAnswer}>
+      //   Answer
+      // </button>
+      null}
+    </Card>
   );
 }
--- async-app/src/components/Jokes.js
+++ async-app/src/components/Jokes.js
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { fetchJokes } from "../actions/jokeActions";
 import Joke from "./Joke";
+import styled from "styled-components";
+
+const Flex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
 
 function Jokes(props) {
   useEffect(() => {
@@ -14,11 +21,11 @@ function Jokes(props) {
       {props.isLoading ? <h4>Finding the BEST jokes...</h4> : null}
       {props.error ? <p>Couldn't find any good jokes... </p> : null}
       {props.jokes.length > 0 ? (
-        <div>
+        <Flex>
           {props.jokes.map((joke) => {
             return <Joke joke={joke} />;
           })}
-        </div>
+        </Flex>
       ) : null}
     </div>
   );
